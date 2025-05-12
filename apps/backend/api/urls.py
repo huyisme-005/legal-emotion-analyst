ef009c3b43c5830cb17b6be7b520946813efcda0
Huy Le <legiahuy050104@gmail.com>
2025-05-11 21:38:27 -0400

    URL routes for the API app created

---
+++ apps/backend/api/urls.py
@@ -0,0 +1,9 @@
+"""
+URL routes for the API app.
+"""
+from django.urls import path
+from .views import AnalyzeView
+
+urlpatterns = [
+    path('analyze/', AnalyzeView.as_view(), name='analyze'),
+]
