4f8b7a502112941f192101ced26a73e0843efb3a
Huy Le <legiahuy050104@gmail.com>
2025-05-14 17:57:20 -0400

    urls.py updated

--- apps/backend/api/urls.py
+++ apps/backend/api/urls.py
@@ -9,7 +9,9 @@ def empty_favicon(request):
     return HttpResponse(status=204)  # No Content
 
 urlpatterns = [
+    path('favicon.ico', empty_favicon),  # Handles favicon requests gracefully, must be before
+    #any catch-all patterns
     path('analyze/', AnalyzeView.as_view(), name='analyze'),
     path('api/', include('api.urls')),
-    path('favicon.ico', empty_favicon),  # Handles favicon requests gracefully
+    
 ]
