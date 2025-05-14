887937a48d41bb892e453db0a4b34c264ca7120e
Huy Le <legiahuy050104@gmail.com>
2025-05-14 18:07:24 -0400

    urls.py for backend updated

--- apps/backend/backend/urls.py
+++ apps/backend/backend/urls.py
@@ -1,8 +1,16 @@
 """
 URL configuration for backend project.
 """
+from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def empty_favicon(request):
+    return HttpResponse(status=204)  # No Content
 
 urlpatterns = [
-    path('api/', include('api.urls')),
+    path('favicon.ico', empty_favicon),  # Handles favicon requests gracefully
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),   # Include your app's URLs here
 ]
+
