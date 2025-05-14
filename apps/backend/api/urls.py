d0503033d1b958a8b6cb0c5fa8df6970ae643a9d
Huy Le <legiahuy050104@gmail.com>
2025-05-14 18:01:34 -0400

    urls.py updated

--- apps/backend/api/urls.py
+++ apps/backend/api/urls.py
@@ -1,17 +1,13 @@
 """
 URL routes for the API app.
 """
-from django.urls import path,include
-from django.http import HttpResponse
+from django.urls import path
 from .views import AnalyzeView
 
-def empty_favicon(request):
-    return HttpResponse(status=204)  # No Content
 
 urlpatterns = [
-    path('favicon.ico', empty_favicon),  # Handles favicon requests gracefully, must be before
-    #any catch-all patterns
+    
     path('analyze/', AnalyzeView.as_view(), name='analyze'),
-    path('api/', include('api.urls')),
+    
     
 ]
